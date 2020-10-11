adebc1a9a44fe8f46a6208fb250dc2b2c7f625cd
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-10-11 13:00:36 +0300

    site

--- site.js
+++ site.js
@@ -8,6 +8,14 @@ var staticSiteOptions = {
 //   maxAge: 60 * 10 //10 сек. - 1000 * 60 * 15 хранить страницы в кэше пятнадцать минут
 };
 
+let abc = 123;
+
+if (abc == 223) {
+   console.log(223);
+} else if (abc == 123) {
+   console.log(123);
+}
+
 // Запуск сайта:
 express().use(express.static(
    path.join(__dirname, 'static'),
