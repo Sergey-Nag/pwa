8fa9e4bbd7a2cf9c3894b62da7f45afea353066e
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-10-11 13:25:41 +0300

    new

--- site.js
+++ site.js
@@ -7,9 +7,7 @@ var staticSiteOptions = {
    portnum: 80, // слушать порт 80
 //   maxAge: 60 * 10 //10 сек. - 1000 * 60 * 15 хранить страницы в кэше пятнадцать минут
 };
-
 let abc = 123;
-
 if (abc == 223) {
    console.log(223);
 } else if (abc == 123) {
